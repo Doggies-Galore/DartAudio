8571f2da3e0bfed3cbbee0998f578fae0e462ce4
Doggies-Galore <68348295+Doggies-Galore@users.noreply.github.com>
2024-04-30 20:15:35 -0600

    The files

---
+++ audio/make_audio_changes_20141219.bat
@@ -0,0 +1,22 @@
+REM ------------------------------------------------------------
+REM  changing spoken from "Union Station" to "Dallas Union Station"
+ren 22748.mp3  22748_OLD_VERSION.mp3
+copy  /-Y  new_recordings_20140925\pn800312.mp3  22748.mp3
+REM ------------------------------------------------------------
+REM  changing spoken "CentrePort DFW Station" to "CentrePort DFW Airport Station"
+ren 28178.mp3  28178_OLD_VERSION.mp3
+copy  /-Y  new_recordings_20140925\pn800406.mp3  28178.mp3
+REM ------------------------------------------------------------
+REM  changing spoken "Hurst Bell Station" to "Bell Station"
+ren 28180.mp3  28180_OLD_VERSION.mp3
+copy  /-Y  new_recordings_20140925\pn800340.mp3  28180.mp3
+REM ------------------------------------------------------------
+REM  changing spoken "T&P Station" to "Fort Worth T&P Station"
+ren 28252.mp3  28252_OLD_VERSION.mp3
+copy  /-Y  new_recordings_20140925\pn800434.mp3  28252.mp3
+REM ------------------------------------------------------------
+
+REM  changing spoken "ITC Station" to "Fort Worth Intermodal Transportation Center Station"
+ren 28251.mp3  28251_OLD_VERSION.mp3
+copy  /-Y  new_recordings_20140925\pn800430.mp3  28251.mp3
+REM ------------------------------------------------------------
---
+++ isolate.py
@@ -0,0 +1,42 @@
+import os
+
+# Function to create dictionaries mapping station IDs to names
+def create_station_mapping(input_file):
+    station_id_to_name = {}
+    station_name_to_id = {}
+    with open(input_file, 'r') as f:
+        for line in f:
+            if 'STATION' in line:
+                parts = line.split(',')
+                station_id = parts[0].strip()
+                station_name = parts[2].strip()
+                station_id_to_name[station_id] = station_name
+                station_name_to_id[station_name] = station_id
+    return station_id_to_name, station_name_to_id
+
+# Function to rename audio files based on station IDs
+def rename_audio_files(directory, station_id_to_name):
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+        if os.path.isfile(filepath):
+            # Check if the file name contains a station ID
+            for station_id, station_name in station_id_to_name.items():
+                if station_id in filename:
+                    # Construct the new file name
+                    new_filename = filename.replace(station_id, station_name.replace("/", "-"))
+                    # Rename the file
+                    os.rename(filepath, os.path.join(directory, new_filename))
+                    print(f"Renamed {filename} to {new_filename}")
+                    break
+
+# Path to the input file containing station IDs and names
+input_file = 'stations.txt'
+
+# Directory containing audio files
+audio_directory = 'audio'
+
+# Create dictionaries mapping station IDs to names
+station_id_to_name, station_name_to_id = create_station_mapping(input_file)
+
+# Rename audio files based on station IDs
+rename_audio_files(audio_directory, station_id_to_name)
